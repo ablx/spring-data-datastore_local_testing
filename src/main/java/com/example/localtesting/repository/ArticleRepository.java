6609c3f8ddff5b0232ad19365a1d8498486604e6
ablx <mirco.franzek@otto.de>
2021-10-19 15:06:13 +0200

    Use maven-exec to start and stop the emulator.

--- src/main/java/com/example/localtesting/repository/ArticleRepository.java
+++ src/main/java/com/example/localtesting/repository/ArticleRepository.java
@@ -2,10 +2,9 @@ package com.example.localtesting.repository;
 
 import com.example.localtesting.model.Article;
 import com.google.cloud.spring.data.datastore.repository.DatastoreRepository;
-import com.google.datastore.v1.Key;
 import java.util.List;
 
-public interface ArticleRepository extends DatastoreRepository<Article, Key> {
+public interface ArticleRepository extends DatastoreRepository<Article, Long> {
 
   List<Article> findAllByTextIsNull();
 
