7d03c4f971212da7b8a5f698f2f1f87bd4c92944
ablx <mirco.franzek@otto.de>
2021-10-19 14:39:28 +0200

    Initial commit

---
+++ src/main/java/com/example/localtesting/DataDatastoreLocalTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.localtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDatastoreLocalTestingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DataDatastoreLocalTestingApplication.class, args);
+  }
+
+}
